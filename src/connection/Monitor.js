38a83183f42259a4bd749349f41a12bc6ede50cd
jpagand <jordane.pagand@epfl.ch>
2013-09-20 11:37:17 +0200

    Added Monitor

---
+++ src/connection/Monitor.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @type {Function}
+ * @constructor
+ */
+var Monitor = module.exports = function(filter) {
+
+};
+
+/**
+ * TODO:
+ * - expose events for data changes
+ * - eventually expose getter/setter to update filter
+ */
