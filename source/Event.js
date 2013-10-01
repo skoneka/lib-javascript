c5dd19ea3c932452635e26daaeee0b85b2883a1b
perki <pm@perki.com>
2013-10-01 22:50:44 +0200

    added error alert when local storage is not activated

--- source/Event.js
+++ source/Event.js
@@ -13,6 +13,9 @@ var Event = module.exports = function (connection, data) {
 
 Object.defineProperty(Event.prototype, 'stream', {
   get: function () {
+    if (! this.connection.datastore) {
+      throw new Error('Activate Datastore to get automatic stream mapping. Or use StreamId');
+    }
     return this.connection.datastore.getStreamById(this.streamId);
   },
   set: function () { throw new Error('Event.stream property is read only'); }
