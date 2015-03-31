ceb59f3fd521675587ad2e8ef0ca9dfd1e8bc368
ikebets <ilia.kebets@epfl.ch>
2015-03-31 22:02:14 +0200

    account tests implemented but don't work yet because a valid access token is required.

--- test/acceptance/node/Connection.account.test.js
+++ test/acceptance/node/Connection.account.test.js
@@ -1,8 +1,8 @@
-/* global describe, it, before, after */
+/* global describe, it, before, after, afterEach */
 var Pryv = require('../../../source/main'),
-    should = require('should'),
-    config = require('../test-support/config.js'),
-    replay = require('replay');
+  should = require('should'),
+  config = require('../test-support/config.js'),
+  replay = require('replay');
 
 // TODO: need a connection with personal access;
 describe('Connection.account', function () {
@@ -17,14 +17,16 @@ describe('Connection.account', function () {
   });
 
   describe('getInfo()', function () {
-    it('must return user account info (e.g. username, email, language)'/*, function (done) {
-        connection.account.getInfo(function (error, result) {
+
+    // TODO: returns error forbidden using current access token
+    it.skip('must return user account info (e.g. username, email, language)', function (done) {
+      connection.account.getInfo(function (error, result) {
         should.not.exist(error);
         should.exist(result);
         result.should.have.properties('username', 'email', 'language');
         done();
-        });
-        }*/);
+      });
+    });
 
     it('must return an error when the connection lacks sufficient permissions', function (done) {
       connection.account.getInfo(function (error) {
@@ -34,8 +36,29 @@ describe('Connection.account', function () {
     });
   });
 
-  describe('changePassword()', function () {
-    it('must change the account password');
+  // TODO: find out what access token is required to change the password
+  describe.skip('changePassword()', function () {
+
+    var newPassword = 'testPassword';
+    var truePassword = 'poilonez';
+
+    afterEach(function (done) {
+      connection.account.changePassword(newPassword, truePassword, function (err, result) {
+        should.exist(result);
+        done(err);
+      });
+    });
+
+    it('must change the account password', function (done) {
+      connection.account.changePassword(truePassword, newPassword, function (err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.should.have.properties('oldPassword', 'newPassword');
+        result.oldPassword.should.eql(truePassword);
+        result.newPassword.should.eql(newPassword);
+        done(err);
+      });
+    });
 
     it('must return an error when the given "old" password is not valid', function (done) {
       connection.account.changePassword('wrongpassword', 'dontcare', function (error) {
@@ -44,7 +67,14 @@ describe('Connection.account', function () {
       });
     });
 
-    it('must return an error when the given "new" password does not match requirements');
+    it('must return an error when the given "new" password does not match requirements',
+      function (done) {
+      var invalidNewPassword = '';
+      connection.account.changePassword(truePassword, invalidNewPassword, function (err) {
+        should.exist(err);
+        done();
+      });
+    });
 
     it('must return an error when the connection lacks sufficient permissions', function (done) {
       connection.account.changePassword('dontcare', 'dontcare', function (error) {
