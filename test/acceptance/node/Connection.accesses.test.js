15053eb45e6f697dbb6311bfdba87aa7542ac3b9
ikebets <ilia.kebets@epfl.ch>
2015-04-14 13:47:42 +0200

    implement accesses.create() tests

--- test/acceptance/node/Connection.accesses.test.js
+++ test/acceptance/node/Connection.accesses.test.js
@@ -3,15 +3,19 @@ var Pryv = require('../../../source/main'),
   should = require('should'),
   config = require('../test-support/config.js'),
   replay = require('replay'),
-async = require('async');
+  async = require('async');
 
 describe('Connection.accesses', function () {
   this.timeout(10000);
 
-  var connection = new Pryv.Connection(config.connectionSettings);
+  var connection;
+
 
   before(function () {
     replay.mode = process.env.REPLAY || 'replay';
+    Pryv.Connection.login(config.loginParams, function (err, newConnection) {
+      connection = newConnection;
+    });
   });
 
   after(function () {
@@ -24,6 +28,11 @@ describe('Connection.accesses', function () {
         should.not.exist(err);
         should.exist(res);
         res.should.be.instanceOf(Array);
+        res.forEach( function (access) {
+          should.exist(access.id);
+          should.exist(access.token);
+          should.exist(access.name);
+        });
         done();
       });
     });
@@ -34,15 +43,18 @@ describe('Connection.accesses', function () {
 
   describe('create()', function () {
 
-    var accessToDelete, testStream;
+    // TODO: find out why it's impossible to create streams with the other connection
+    var accessToDelete, testStream, streamConnection;
 
     before(function (done) {
       testStream = {
         id: 'accessTestStream',
         name: 'accessTestStream'
       };
-      connection.streams.create(testStream, function (err, newStream) {
+      streamConnection = new Pryv.Connection(config.connectionSettings);
+      streamConnection.streams.create(testStream, function (err, newStream) {
         testStream = newStream;
+        console.log(require('util').inspect(err, {depth: null}));
         done(err);
       });
     });
@@ -50,19 +62,18 @@ describe('Connection.accesses', function () {
     after(function (done) {
       async.series([
         function (stepDone) {
-          connection.accesses.delete(accessToDelete.id, function (err, result) {
-            console.log(result);
+          connection.accesses.delete(accessToDelete.id, function (err) {
             stepDone(err);
           });
         },
         function (stepDone) {
-          connection.streams.delete(testStream, function (err, trashedStream) {
+          streamConnection.streams.delete(testStream, function (err, trashedStream) {
             testStream = trashedStream;
             stepDone(err);
           });
         },
         function (stepDone) {
-          connection.streams.delete(testStream, function (err) {
+          streamConnection.streams.delete(testStream, function (err) {
             stepDone(err);
           });
         }
@@ -70,16 +81,16 @@ describe('Connection.accesses', function () {
     });
 
     it('must return the created access', function (done) {
-      var access = {
+      var testAccess = {
         type: 'shared',
         name: 'testAccess',
         permissions: [
-        {
-          streamId: testStream.id,
-          level: 'read'
-        }
-      ]};
-      connection.accesses.create(access, function (err, newAccess) {
+          {
+            streamId: testStream.id,
+            level: 'read'
+          }
+        ]};
+      connection.accesses.create(testAccess, function (err, newAccess) {
         should.not.exist(err);
         should.exist(newAccess);
         accessToDelete = newAccess;
@@ -88,7 +99,15 @@ describe('Connection.accesses', function () {
     });
 
     it('must return an error if the new access\'s parameters are invalid', function (done) {
-      var invalidAccess = {};
+      var invalidAccess = {
+        type: 'wrongType',
+        name: 'wrongAccess',
+        permissions: [
+          {
+            streamId: testStream.id,
+            level: 'wrongLevel'
+          }
+        ]};
       connection.accesses.create(invalidAccess, function (err) {
         should.exist(err);
         done();
@@ -97,28 +116,70 @@ describe('Connection.accesses', function () {
 
   });
 
-  describe.skip('update()', function () {
+  describe('update()', function () {
 
-    var testAccess;
+    var testAccess, testStream, streamConnection;
 
     before(function (done) {
-      testAccess = {
-
+      testStream = {
+        id: 'accessTestStream',
+        name: 'accessTestStream'
       };
-      connection.accesses.create(testAccess, function (err, newAccess) {
-        testAccess = newAccess;
-        done(err);
-      });
+      testAccess = {
+        type: 'shared',
+        name: 'testAccess',
+        permissions: [
+          {
+            streamId: testStream.id,
+            level: 'read'
+          }
+        ]};
+
+      streamConnection = new Pryv.Connection(config.connectionSettings);
+
+      async.series([
+        function (stepDone) {
+          streamConnection.streams.create(testStream, function (err, newStream) {
+            testStream = newStream;
+            stepDone(err);
+          });
+        },
+        function (stepDone) {
+          connection.accesses.create(testAccess, function (err, newAccess) {
+            testAccess = newAccess;
+            console.log(newAccess);
+            stepDone(err);
+          });
+        }
+      ], done);
+
     });
 
     after(function (done) {
-      connection.accesses.delete(testAccess.id, function (err) {
-        done(err);
-      });
+      async.series([
+        function (stepDone) {
+          connection.accesses.delete(testAccess.id, function (err) {
+            stepDone(err);
+          });
+        },
+        function (stepDone) {
+          streamConnection.streams.delete(testStream, function (err, trashedStream) {
+            testStream = trashedStream;
+            stepDone(err);
+          });
+        },
+        function (stepDone) {
+          streamConnection.streams.delete(testStream, function (err) {
+            stepDone(err);
+          });
+        }
+      ], done);
     });
 
     it('must return the updated access', function (done) {
       testAccess.name = 'myNewAccessName';
+      console.log('gonna update da access');
+      console.log(testAccess);
       connection.accesses.update(testAccess, function (err, updatedAccess) {
         should.not.exist(err);
         should.exist(updatedAccess);
