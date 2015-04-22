480b0b2b0c52337989f323b5daf7ec0e9d3b191d
ikebets <ilia.kebets@epfl.ch>
2015-04-22 16:57:20 +0200

    implement tests for getPrivate() and getPublic() for ConnectionProfile

--- test/acceptance/node/Connection.profile.test.js
+++ test/acceptance/node/Connection.profile.test.js
@@ -1,15 +1,19 @@
 /* global describe, it, before, after */
-var //Pryv = require('../../../source/main'),
-  //should = require('should'),
-  //config = require('../test-support/config.js'),
+var Pryv = require('../../../source/main'),
+  should = require('should'),
+  config = require('../test-support/config.js'),
   replay = require('replay');
 
 describe('Connection.profile', function () {
-  this.timeout(20000);
-  //var connection = new Pryv.Connection(config.connectionSettings);
+  this.timeout(10000);
+  var connection;
 
-  before(function () {
+  before(function (done) {
     replay.mode = process.env.REPLAY || 'replay';
+      Pryv.Connection.login(config.loginParams, function (err, newConnection) {
+        connection = newConnection;
+        done(err);
+      });
   });
 
   after(function () {
@@ -18,17 +22,31 @@ describe('Connection.profile', function () {
 
   describe('getPrivate()', function () {
 
-    it('must return the user\'s private profile set');
+    it('must return the user\'s private profile set', function (done) {
+      connection.profile.getPrivate(null, function (err, res) {
+        should.not.exist(err);
+        should.exist(res);
+        console.log('privateProfile: ', res);
+        done(err);
+      });
+    });
   });
 
   describe('getPublic()', function () {
 
-    it('must return the user\'s current public profile set');
+    it('must return the user\'s current public profile set', function (done) {
+      var connection = new Pryv.Connection(config.connectionSettings);
+      connection.profile.getPublic(null, function (err, res) {
+        should.not.exist(err);
+        should.exist(res);
+        console.log('public profile: ', res);
+        done();
+      });
+    });
   });
 
   describe('setPrivate()', function () {
-
-
+    
   });
 
   describe('setPublic()', function () {
